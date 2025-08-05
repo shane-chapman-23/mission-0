eb64546ab8fa6c39c90486d30f37dd1ed801a024
shane-chapman-23 <chapmanshane23@gmail.com>
2025-08-05 15:51:21 +1200

    added articles array and displayed it inside the articleDisplay component

--- src/Home/ArticleDisplay/ArticleDisplay.jsx
+++ src/Home/ArticleDisplay/ArticleDisplay.jsx
@@ -1,5 +1,59 @@
 import styles from "./ArticleDisplay.module.css";
 
 export default function ArticleDisplay() {
-  return <div></div>;
+  const articles = [
+    {
+      title: "Article Title",
+      image: "/images/big-chillin.png",
+      preview: "This is a preview for the article",
+      datePublished: "May 12, 2025",
+    },
+    {
+      title: "Article Title",
+      image: "/images/big-chillin.png",
+      preview: "This is a preview for the article",
+      datePublished: "May 12, 2025",
+    },
+    {
+      title: "Article Title",
+      image: "/images/big-chillin.png",
+      preview: "This is a preview for the article",
+      datePublished: "May 12, 2025",
+    },
+    {
+      title: "Article Title",
+      image: "/images/big-chillin.png",
+      preview: "This is a preview for the article",
+      datePublished: "May 12, 2025",
+    },
+    {
+      title: "Article Title",
+      image: "/images/big-chillin.png",
+      preview: "This is a preview for the article",
+      datePublished: "May 12, 2025",
+    },
+    {
+      title: "Article Title",
+      image: "/images/big-chillin.png",
+      preview: "This is a preview for the article",
+      datePublished: "May 12, 2025",
+    },
+  ];
+  return (
+    <div className={styles.articleDisplayContainer}>
+      <div className={styles.articleDisplayGrid}>
+        {articles.map((article) => {
+          return (
+            <div className={styles.articlePreviewContainer}>
+              <img src={article.image}></img>
+              <h1>{article.title}</h1>
+              <p>{article.datePublished}</p>
+              <hr></hr>
+              <p>{article.preview}...</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
